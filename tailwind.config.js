8303acc5d87a9ddcb8e8980cf95f89a49cd462b3
teslim <tunjiheritage@gmail.com>
2024-12-22 12:02:04 +0100

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html', './*.js'],
+  content: ['./public/**/*.html', './*.js'],
   theme: {
     extend: {
-     
+
     },
-    fontFamily:{
-      Outfit:["Outfit", "sans-serif"],
-      Ovo:["Ovo", "serif"]
+    fontFamily: {
+      Outfit: ["Outfit", "sans-serif"],
+      Ovo: ["Ovo", "serif"]
     }
   },
   plugins: [],
